295438145e755be909dd663853e24d37c153cad6
gly <glefranc@eig.fr>
2023-01-12 14:48:51 +0100

    Split args from main + add arguments

---
+++ src/arguments.rs
@@ -0,0 +1,30 @@
+use clap::{Parser, ValueEnum};
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
+pub enum Formatter {
+    /// Output the tree into a regex
+    Regex,
+    /// Output the tree into a json
+    Json,
+    /// Output the tree into rust ast
+    Rust,
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    /// Path to the file containing the list of words
+    /// Defaults to stdin
+    #[arg(short, long)]
+    pub input: Option<String>,
+    /// Path to the file where the output will be written
+    /// Defaults to stdout
+    #[arg(short, long)]
+    pub output: Option<String>,
+    /// Format of the output
+    #[arg(long)]
+    pub format: Formatter,
+    /// Pretty print the output
+    #[arg(long)]
+    pub pretty: bool,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
-use clap::Parser;
 mod node;
-#[derive(clap::Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-   /// File with the list to load
-   #[arg(short, long)]
-   file: String,
-}
+mod regex;
+mod arguments;
+
+use std::io::{Read, Write};
+use clap::Parser;
+use arguments::Args;
 
 fn main() {
     let args = Args::parse();
-    let list_str = std::fs::read_to_string(&args.file).expect("Unable to read file");
+    let list_str = match args.input {
+        Some(path) => std::fs::read_to_string(path).expect("Unable to read file"),
+        None => {
+            let mut list_str = String::new();
+            std::io::stdin().read_to_string(&mut list_str).expect("Unable to read stdin");
+            list_str
+        }
+    };
     let mut list = list_str.lines().collect::<Vec<_>>();
     list.sort();
     let root = node::Node::new(list.as_slice());
-    // println!("{:#?}", root);
+    let mut file : Box<dyn Write> = match args.output {
+        Some(path) => Box::new(std::fs::File::create(path).expect("Unable to create file")),
+        None => Box::new(std::io::stdout())
+    };
+    let output = match args.format {
+        arguments::Formatter::Json => "Work in progress".to_string(),
+        arguments::Formatter::Regex => regex::Regex::from(root).0,
+        arguments::Formatter::Rust => if args.pretty {
+            format!("{:#?}", root)
+        } else {
+            format!("{:?}", root)
+        }
+    };
+    file.write_all(output.as_bytes()).expect("Unable to write to file");
 }
