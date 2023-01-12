d8d817111b2ffbb7d54c9c21058951ef8d00c998
gly <glefranc@eig.fr>
2023-01-12 14:24:23 +0100

    add regex formatter

---
+++ src/regex.rs
@@ -0,0 +1,55 @@
+use crate::node::Node;
+
+#[derive(Debug)]
+pub struct Regex(pub String);
+
+impl Regex {
+    pub fn new(regex: &str) -> Self {
+        Self(regex.to_string())
+    }
+    fn push_from_node(str: &mut String, node: &Node) {
+        match node {
+            Node::Leaf(word) => {
+                str.push_str(word);
+            }
+            Node::Branch(word, nodes) => {
+                str.push_str(word);
+                str.push_str("(?:");
+                let mut has_empty = false;
+                for node in nodes {
+                    let str_length = str.len();
+                    Self::push_from_node(str, node);
+                    if str_length == str.len() {
+                        has_empty = true;
+                        continue;
+                    }
+                    str.push('|');
+                }
+                if !has_empty {
+                    str.pop();
+                }
+                str.push(')');
+            }
+        }
+    }
+    fn compute_length(node: &Node) -> usize {
+        match node {
+            Node::Leaf(word) => word.len(),
+            Node::Branch(word, nodes) => {
+                let mut length = word.len() + 3;
+                for node in nodes {
+                    length += Self::compute_length(node)+1;
+                }
+                length
+            }
+        }
+    }
+}
+
+impl From<Node> for Regex {
+    fn from(node: Node) -> Self {
+        let mut regex = String::with_capacity(Self::compute_length(&node));
+        Self::push_from_node(&mut regex, &node);
+        Self(regex)
+    }
+}
