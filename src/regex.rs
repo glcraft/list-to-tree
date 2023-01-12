8f96b5a1561160dfb1fe3939a63d8117ff7ea9ee
gly <glefranc@eig.fr>
2023-01-12 18:06:35 +0100

    Adjust and improove regex formatter

--- src/regex.rs
+++ src/regex.rs
@@ -4,21 +4,20 @@ use crate::node::Node;
 pub struct Regex(pub String);
 
 impl Regex {
-    fn push_from_nodes(str: &mut String, nodes: &[Node])
+    fn push_from_nodes(str: &mut String, nodes: &[Node]) -> usize
     {
-        let mut has_empty = false;
+        let mut nword = 0;
         for node in nodes {
             let str_length = str.len();
             Self::push_from_node(str, node);
             if str_length == str.len() {
-                has_empty = true;
                 continue;
             }
+            nword += 1;
             str.push('|');
         }
-        if !has_empty {
-            str.pop();
-        }
+        str.pop();
+        nword
     }
 
     fn push_from_node(str: &mut String, node: &Node) {
@@ -32,6 +31,12 @@ impl Regex {
                 Self::push_from_nodes(str, nodes.as_slice());
                 str.push(')');
             }
+            Node::BranchIncluded(word, nodes) => {
+                str.push_str(word);
+                str.push_str("(?:");
+                Self::push_from_nodes(str, nodes.as_slice());
+                str.push_str(")?");
+            }
         }
     }
     fn compute_length_from_nodes(nodes: &[Node]) -> usize {
@@ -44,7 +49,8 @@ impl Regex {
     fn compute_length_from_node(node: &Node) -> usize {
         match node {
             Node::Leaf(word) => word.len(),
-            Node::Branch(word, nodes) => word.len() + 4 + Self::compute_length_from_nodes(nodes.as_slice())
+            Node::Branch(word, nodes) => word.len() + 4 + Self::compute_length_from_nodes(nodes.as_slice()),
+            Node::BranchIncluded(word, nodes) => word.len() + 5 + Self::compute_length_from_nodes(nodes.as_slice())
         }
     }
 }
@@ -53,11 +59,15 @@ impl From<Node> for Regex {
     fn from(node: Node) -> Self {
         match node {
             Node::Leaf(word) => Self(word.to_string()),
+            ref node @ Node::BranchIncluded(_,_) => {
+                let mut regex = String::with_capacity(Self::compute_length_from_node(&node));
+                Self::push_from_node(&mut regex, &node);
+                Self(regex)
+            }
             ref node @ Node::Branch(ref word, ref children) => {
                 if word.is_empty() {
                     let mut regex = String::with_capacity(Self::compute_length_from_nodes(children.as_slice()));
                     Self::push_from_nodes(&mut regex, children.as_slice());
-                    regex.pop();
                     Self(regex)
                 } else {
                     let mut regex = String::with_capacity(Self::compute_length_from_node(&node));
